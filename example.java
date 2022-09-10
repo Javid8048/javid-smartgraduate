9d5a21737e5bee2e1b2f1fdab5c3267e3bc7e3ca
Javid8048 <112797329+Javid8048@users.noreply.github.com>
2022-09-10 22:56:00 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,9 @@
+public class example {
+	int x=5;
+	public static void main(String args[]) {
+		example s = new example();
+		example q = new example();
+		System.out.println(s, x);
+		System.out.println(q, x);
+	}
+}
